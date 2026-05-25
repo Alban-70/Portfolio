a3f6f22f51e9e5d0dbb858539cb5d7cc2f327627
achoule3 <alban.choulet@edu.univ-fcomte.fr>
2026-05-25 17:06:33 +0200

    ajout du premier savoir-faire

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,8 @@
         <div class="content_nav">
             <router-link class="pointer" :to="{ name: 'Home' }">Accueil</router-link>
             <router-link class="pointer" :to="{ name: 'Technique' }">Technique</router-link>
-            <router-link class="pointer">Suivi de projet</router-link>
-            <router-link class="pointer">Intégration en entreprise</router-link>
+            <router-link class="pointer" :to="{  }">Suivi de projet</router-link>
+            <router-link class="pointer" :to="{  }">Intégration en entreprise</router-link>
         </div>
         <div class="nav_spacer"></div>
     </div>
